64719a16c9a4c8d23e7b7b222fc77c3fcded881f
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-05-25 21:30:05 -0400

    Adicionando uma tela de login customizada

--- curso.springboot/src/main/java/curso/springboot/security/WebConfigSecurity.java
+++ curso.springboot/src/main/java/curso/springboot/security/WebConfigSecurity.java
@@ -15,31 +15,33 @@ import curso.springboot.service.ImplementacaoUserDatailsService;
 
 @Configuration
 @EnableWebSecurity
-public class WebConfigSecurity extends WebSecurityConfigurerAdapter{
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private ImplementacaoUserDatailsService implementacaoUserDatailsService;
-	
-	@Override //Configura as solicitações de acesso por http
+
+	@Override // Configura as solicitações de acesso por http
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable() //desativa as configurações padrão de memória (spring)
-		.authorizeRequests() //restringir acessos
-		.antMatchers(HttpMethod.GET,"/").permitAll() //Qualquer usuário acessa a página inicial
-		.antMatchers(HttpMethod.GET,"/cadastropessoa").hasRole("ADMIN") //Somente admin vai acessar está página
-		.anyRequest().authenticated()
-		.and().formLogin().permitAll() //permite qualquer usuário 
-		.and().logout() //Mapeia url de saida e invalida usuário autenticado
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout")); 
-		
+		http.csrf().disable() // desativa as configurações padrão de memória (spring)
+				.authorizeRequests() // restringir acessos
+				.antMatchers(HttpMethod.GET, "/").permitAll() // Qualquer usuário acessa a página inicial
+				.antMatchers(HttpMethod.GET, "/cadastropessoa").hasRole("ADMIN") // Somente admin vai acessar está
+																					// página
+				.anyRequest().authenticated().and().formLogin().permitAll() // permite qualquer usuário
+				.loginPage("/login").defaultSuccessUrl("/cadastropessoa").failureUrl("/login?error=true").and().logout()
+				.logoutSuccessUrl("/login") // Mapeia url de saida e invalida usuário autenticado
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+
 	}
-	@Override //cria autenticação do usuário com BD ou em memória
+
+	@Override // cria autenticação do usuário com BD ou em memória
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
+
 		auth.userDetailsService(implementacaoUserDatailsService).passwordEncoder(new BCryptPasswordEncoder());
-		
+
 	}
-	
-	@Override //ignora URL especificas
+
+	@Override // ignora URL especificas
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/materialize/**");
 	}
